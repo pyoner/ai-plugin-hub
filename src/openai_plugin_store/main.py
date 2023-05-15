f606d8692a1c8dcc0997c2dd4e82471e9340fa04
Askar Yusupov <devex.soft@gmail.com>
2023-05-15 17:05:50 +0500

    feat: add support for loading plugin manifest from environment variable feat: add endpoint to list plugin manifest deps: add python-dotenv to manage environment variables

--- src/openai_plugin_store/main.py
+++ src/openai_plugin_store/main.py
@@ -1,8 +1,19 @@
+import json
+from os import environ
+from dotenv import load_dotenv
 from fastapi import FastAPI
 
+load_dotenv()
 app = FastAPI()
 
 
 @app.get("/")
 async def root():
-    return {"message": "Hello World" }
+    return {"message": "Hello World"}
+
+
+@app.get("/plugins", summary="List of plugin manifest")
+async def plugins():
+    fields = ["manifest", "categories"]
+    plugins = json.loads(environ["PLUGIN_FILE"])
+    return [dict([(k, item[k]) for k in fields]) for item in plugins["items"]]
