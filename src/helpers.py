9c0705e71e0419b06ef7b7b00954defbf0f8c55a
Askar Yusupov <devex.soft@gmail.com>
2023-05-16 11:33:57 +0500

    feat(helpers.py): add sentence transformer model and embedding function for training and querying feat(helpers.py): add function to create manifest url feat(scripts.py): add new file for future scripts to be added to the project

--- src/helpers.py
+++ src/helpers.py
@@ -1,2 +1,13 @@
+from sentence_transformers import SentenceTransformer
+
+name = "paraphrase-albert-small-v2"
+model = SentenceTransformer(name)
+
+
+# used for both training and querying
+def embed_func(batch):
+    return [model.encode(sentence) for sentence in batch]
+
+
 def create_manifest_url(domain: str):
     return "https://{domain}/.well-known/ai-plugin.json".format(domain=domain)
