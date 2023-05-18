b77d1c40963d9cd1f0415ddd653cbaf88d6a99f0
Askar Yusupov <devex.soft@gmail.com>
2023-05-18 14:04:59 +0500

    refactor(scripts.py): remove hardcoded table name and use a constant variable instead feat(scripts.py): add functionality to delete the database directory before creating a new table to avoid conflicts with previous data

--- src/scripts.py
+++ src/scripts.py
@@ -1,3 +1,5 @@
+import os
+import shutil
 import pandas as pd
 
 from lancedb.embeddings import with_embeddings
@@ -8,8 +10,12 @@ from .helpers import db_connect, embed_func, load_manifests
 
 load_dotenv()
 
+PLUGINS_TABLE_NAME = "plugins"
+
 
 def prepare():
+    shutil.rmtree(os.environ["LANCE_DB"])
+
     manifests = load_manifests()
     df = pd.DataFrame(data=[m.dict() for m in manifests])
     df = with_embeddings(
@@ -22,12 +28,12 @@ def prepare():
     db = db_connect()
 
     # The output is used to create / append to a table
-    return db.create_table("plugins", data=df)
+    return db.create_table(PLUGINS_TABLE_NAME, data=df)
 
 
 def search():
     db = db_connect()
-    t = db.open_table("plugins")
+    t = db.open_table(PLUGINS_TABLE_NAME)
 
     query = "Find plugins to work with documents, pdf, sheets"
     query_vec = embed_func([query])[0]
